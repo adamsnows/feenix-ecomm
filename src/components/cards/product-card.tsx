ba19abc70417ddd6c0dd8b764c97af7784502ae1
Adam N <adaamangelow@gmail.com>
2024-08-17 15:48:08 -0300

    fix: wrong title size for product cards

--- src/components/cards/product-card.tsx
+++ src/components/cards/product-card.tsx
@@ -10,15 +10,17 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <Link className="h-[400px] w-full" href={`/product/${product.id}`}>
-      <div className="bg-white w-full  p-10">
+      <div className="bg-white w-full p-10">
         <img
           src={product.image}
           alt={product.title}
           className="mx-auto h-[250px] object-contain"
         />
       </div>
-      <div className="flex flex-col mt-2">
-        <span className="font-bold">{product.title}</span>
+      <div className="flex flex-col mt-2 px-2">
+        <span className="font-bold text-base overflow-hidden overflow-ellipsis whitespace-nowrap">
+          {product.title}
+        </span>
         <span className="text-sm text-gray-600 capitalize">
           {product.category}
         </span>
